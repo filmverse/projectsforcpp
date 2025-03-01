f8f5cc4f9a1536d748c524652649c1f0524c5e1e
VIKAS SHARMA <vkassharma19492@gmail.com>
2025-03-02 01:43:31 +0530

    c

--- helloworld/helloc.c
+++ helloworld/helloc.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+void vikas() {
+    printf("hello world in my void function \n");
+}
+
 int main() {
     int a = 10;
+    vikas();
     printf("%d \n", a);
-
+    vikas();
     return 0;
 }
