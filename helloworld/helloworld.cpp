8d626540be9a89b519f52501c71d86ad17d89380
VIKAS SHARMA <vkassharma19492@gmail.com>
2025-03-01 14:56:15 +0530

    aa

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,18 +1,8 @@
+// just printing hello world
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-
+int main() {
+    cout << "Hello World!";
     return 0;
 }
