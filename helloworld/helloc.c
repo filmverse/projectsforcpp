c88d6a97dc641216fb09cae2f61ff4bbcc55eb3a
VIKAS SHARMA <vkassharma19492@gmail.com>
2025-03-03 01:38:47 +0530

    message

--- helloworld/helloc.c
+++ helloworld/helloc.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-void vikas() {
+void vikas(int val) {
     printf("hello world in my void function \n");
+    printf("getting value from parent function \n");
+    printf("and the value is %d \n", val);
+
 }
 
 int main() {
     int a = 10;
-    vikas();
+    int b = 20;
+    int c = 30;
+    vikas(b);
     printf("%d \n", a);
-    vikas();
+    vikas(c);
     return 0;
 }
