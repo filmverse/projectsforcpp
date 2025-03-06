89ddebe2c732581e60e7a1a91699a5e011c32625
Vikas Sharma <vkassharma19492@gmail.com>
2025-03-07 01:45:31 +0530

    c

--- helloworld/helloc.c
+++ helloworld/helloc.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 void vikas(int val) {
+    int i = 1;
     printf("hello world in my void function \n");
     printf("getting value from parent function \n");
     printf("and the value is %d \n", val);
+    printf("%d %d %d\n", i++, i++, i);
 
 }
 
@@ -16,3 +18,12 @@ int main() {
     vikas(c);
     return 0;
 }
+
+
+// int main(int argc, char *argv[]) {
+//     printf("Number of arguments: %d\n", argc);
+//     for (int i = 0; i < argc; i++) {
+//         printf("Argument %d: %s\n", i, argv[i]);
+//     }
+//     return 0;
+// }
